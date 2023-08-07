1e74d7225e36c63aee8970a235bb75bf6dc58b72
unknown <jyygxxf@outlook.com>
2023-08-07 22:21:04 +0800

    void * to char *

--- my_malloc2.c
+++ my_malloc2.c
@@ -2,7 +2,7 @@
 
 u8 memory[MEMORY_LENGTH];
 
-#define is_legal(x) ((void *)(x) < (void *)memory + sizeof(memory))
+#define is_legal(x) ((char *)(x) < (char *)memory + sizeof(memory))
 
 #pragma pack(1)
 struct _malloc_struct
@@ -32,19 +32,19 @@ void *my_malloc(size_t length)
     while (!check_this_struct(p, length))
     {
         previous_p = p;
-        p = (void *)p + p->after;
-        if ((void *)p >= (void *)memory + MEMORY_LENGTH)
+        p = (struct _malloc_struct *)((char *)p + p->after);
+        if ((char *)p >= (char *)memory + MEMORY_LENGTH)
             return 0; // 满了
     }
 
     // 找到
     p->is_malloc = 1;
     p->before = previous_p == NULL ? 0 : previous_p->after;
-    later_p = (void *)p + p->after;
-    insert_p = (void *)p + sizeof(struct _malloc_struct) + length; // 创建一个later结构malloc数据结构
+    later_p = (struct _malloc_struct *)((char *)p + p->after);
+    insert_p = (struct _malloc_struct *)((char *)p + sizeof(struct _malloc_struct) + length); // 创建一个later结构malloc数据结构
     if (insert_p != later_p)
     {
-        //可以容得下insert_p
+        // 可以容得下insert_p
         insert_p->is_malloc = 0;
         insert_p->before = sizeof(struct _malloc_struct) + length;
         insert_p->after = p->after - sizeof(struct _malloc_struct) - length;
@@ -52,18 +52,19 @@ void *my_malloc(size_t length)
             later_p->before = insert_p->after;
         p->after = insert_p->before;
     }
-    return (void *)p + sizeof(struct _malloc_struct);
+    return (char *)p + sizeof(struct _malloc_struct);
 }
 
 void my_free(void *p)
 {
-    struct _malloc_struct *s = p - sizeof(struct _malloc_struct);
-    struct _malloc_struct *previous = (void *)s - s->before, *later = (void *)s + s->after;
+    struct _malloc_struct *s = (struct _malloc_struct *)((char *)p - sizeof(struct _malloc_struct));
+    struct _malloc_struct *previous = (struct _malloc_struct *)((char *)s - s->before),
+                          *later = (struct _malloc_struct *)((char *)s + s->after);
     s->is_malloc = 0;
     if (is_legal(later) && !later->is_malloc) // later存在且空
     {
         s->after += later->after;
-        later = (void *)s + s->after;
+        later = (struct _malloc_struct *)((char *)s + s->after);
         if (is_legal(later))
             later->before = s->after;
     }
@@ -78,7 +79,7 @@ void my_free(void *p)
 u16 whether_can_malloc_the_size(u16 length)
 {
     struct _malloc_struct *p = (struct _malloc_struct *)memory;
-    while ((void *)p - (void *)memory < MEMORY_LENGTH)
+    while ((char *)p - (char *)memory < MEMORY_LENGTH)
     {
         if (!p->is_malloc)
         {
@@ -87,7 +88,7 @@ u16 whether_can_malloc_the_size(u16 length)
             if (length + 2 * sizeof(struct _malloc_struct) <= p->after)
                 return 1;
         }
-        p = (void *)p + p->after;
+        p = (struct _malloc_struct *)((char *)p + p->after);
     }
     return 0;
 }
